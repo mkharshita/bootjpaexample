c58c88598afb717cb6a6d61cbb3ec24a62f78f5f
mkharshita <mkharshita5@gmail.com>
2024-09-28 15:05:07 +0530

    get All users

--- src/main/java/com/jpa/test/BootjpaexampleApplication.java
+++ src/main/java/com/jpa/test/BootjpaexampleApplication.java
@@ -1,6 +1,7 @@
 package com.jpa.test;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,23 +23,39 @@ public class BootjpaexampleApplication {
 //		User daoUser = userRepository.save(user);
 //		System.out.println(daoUser);
 		
-		User user1 = new User();
-		user1.setName("Akshat");
-		user1.setCity("Noida");
-		user1.setStatus("Golang Developer");
+//		User user1 = new User();
+//		user1.setName("Akshat");
+//		user1.setCity("Noida");
+//		user1.setStatus("Golang Developer");
+//		
+//		
+//		User user2 = new User();
+//		user2.setName("Shruti");
+//		user2.setCity("Delhi");
+//		user2.setStatus("DevOps");
+//		
+//		List<User> users = List.of(user1,user2);
+//		Iterable<User> iUsers = userRepository.saveAll(users);
+//		
+//		iUsers.forEach(user -> System.out.println(user));
+		
+		//Update - User data
+//		Optional<User> optional = userRepository.findById(1);
+//		User user = optional.get();
+//		user.setStatus("Backend Developer");
+//		User result= userRepository.save(user);
+//		
+//		System.out.println(result);
 		
 		
-		User user2 = new User();
-		user2.setName("Shruti");
-		user2.setCity("Delhi");
-		user2.setStatus("DevOps");
-		
-		List<User> users = List.of(user1,user2);
-		Iterable<User> iUsers = userRepository.saveAll(users);
+		//Get Data
+		Iterable<User> iUsers = userRepository.findAll();
 		
 		iUsers.forEach(user -> System.out.println(user));
 		
-		System.out.println("Done");
+		
+		
+		
 		
 	}
 
